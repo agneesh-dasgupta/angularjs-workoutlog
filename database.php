941048e83cd1493095b2a02024b493a728cb07a3
Danny Aguirre <daniel.aguirreduran@wustl.edu>
2018-04-24 21:59:48 -0500

    added actual db info

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
 // Content of database.php
 //creation of mysqli varibale to be used in all of the queries
-$mysqli = new mysqli('localhost', 'newsadmin', 'fakenews', 'news');
+$mysqli = new mysqli('localhost', 'dannyAguirre', 'dA482521440', 'snake');
 
 if($mysqli->connect_errno) {
 	printf("Connection Failed: %s\n", $mysqli->connect_error);
